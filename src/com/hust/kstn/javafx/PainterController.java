763bbd1d784aed748b774d4f476c8cc9ecb56a49
Linh126-minari <hirofumiyoshida2712@gmail.com>
2025-12-18 09:18:32 +0700

    add GUI project

---
+++ src/com/hust/kstn/javafx/PainterController.java
@@ -0,0 +1,43 @@
+package com.hust.kstn.javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class PainterController {
+
+    @FXML
+    private Pane drawingAreaPane;
+    
+    @FXML
+    private RadioButton eraserRadioButton; 
+    
+    @FXML
+    private RadioButton penRadioButton;    
+
+    @FXML
+    private ToggleGroup toolGroup;
+    
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+    	Color newColor = Color.BLACK;
+    	
+    	if (eraserRadioButton.isSelected()) {
+    		newColor = Color.WHITE;
+    	}
+    	
+        Circle newCircle = new Circle(event.getX(), event.getY(), 4, newColor);
+        drawingAreaPane.getChildren().add(newCircle);
+    }
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+    	drawingAreaPane.getChildren().clear();
+    }
+
+}
